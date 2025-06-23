a1902a1222fa1f112414528454f75b4a5887c3b0
Jesús Hernández <88148591+yizu13@users.noreply.github.com>
2025-06-23 08:19:59 -0400

    studentsFeatures

--- sidecp_web/src/Dashboard/Usuarios/delegationsLayout.tsx
+++ sidecp_web/src/Dashboard/Usuarios/delegationsLayout.tsx
@@ -1,12 +1,12 @@
 import MainLayout from "../layout/Layout"
-
+import DelegationsComp_ from "./delegations/delegationsComponent"
 
 
 
 export default function DelegationsComp(){
     return(
-        <MainLayout setPage={{page: 2, subPage: 2}}>
-            <></>
+        <MainLayout setPage={{page: 2, subPage: 0}}>
+            <DelegationsComp_/>
         </MainLayout>
     )
 }
